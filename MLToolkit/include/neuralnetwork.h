427f40e2815b8af0a2299d3da8cca14213bb2d0a
Matthew Holmes <matthew.holmes111@gmail.com>
2022-10-31 15:06:26 +0000

    Skeleton header for neural network

---
+++ MLToolkit/include/neuralnetwork.h
@@ -0,0 +1,18 @@
+// this header file defines the NeuralNetwork class
+#ifndef MLTOOLKIT_INCLUDE_NEURALNETWORK_H_
+#define MLTOOLKIT_INCLUDE_NEURALNETWORK_H_
+
+#include <vector>
+
+namespace mltoolkit {
+
+class NeuralNetwork {
+public:
+	NeuralNetwork() {};
+private:
+
+};
+
+} // namespace mltoolkit
+
+#endif
