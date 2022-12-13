373d501f53147b1c138af61b1e043d3eeae73f29
Matthew Holmes <matthew.holmes111@gmail.com>
2022-12-13 16:36:13 +0000

    Trainer classes now own the datasets they use (via unique_ptr)

--- MLToolkit/include/trainer.h
+++ MLToolkit/include/trainer.h
@@ -7,29 +7,27 @@
 
 #include <utility>
 #include <iostream>
+#include <memory>
 
 namespace mltoolkit {
-
-// TODO decide how Data works
-// hold a pointer of make a new version
-// pehaps use a unique ptr to transfer ownership of the data to the class
-// since data follows a stream model, essentially a ifstream in a wrapper for FileData
 
 template <typename MOD>
 class Trainer {
 public:
-	Trainer(MOD mod, Data train, Data test)
-		: model(mod), train_data(train), test_data(test) {}
-	Trainer(MOD mod, ModelMutator<MOD> mut, Data train, Data test)
-		: model(mod), model_mut(mut) train_data(train), test_data(test) {}
+	Trainer(MOD mod, std::unique_ptr<Data> train, std::unique_ptr<Data> test)
+		: model(mod), train_data(std::move(train)), test_data(std::move(test)) {}
+	Trainer(MOD mod, ModelMutator<Mod> mut,
+			std::unique_ptr<Data> train, std::unique_ptr<Data> test)
+		: model(mod), model_mut(mut), 
+		  train_data(std::move(train)), test_data(std::move(test)) {}
 
 	void do_training();
 	void evaluate();
 private:
 	MOD model; // works on a copy of the model passed
 	ModelMutator<MOD> model_mut;
-	Data train_data;
-	Data test_data;
+	std::unique_ptr<Data> train_data_uptr;
+	std::unique_ptr<Data> test_data_uptr;
 };
 
 
